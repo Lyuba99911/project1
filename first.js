2a4695fec4028627aec77b0cce444849a64e1034
Lyuba <example@gmail.com>
2020-04-26 19:14:11 +0300

    first commit

---
+++ first.js
@@ -0,0 +1,44 @@
+'use strict'
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    IsMarried: false
+};
+console.log(person.name);
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[2]);
+//alert('Hello world!');
+
+//let answer = confirm("Are you here?");
+//console.log(answer);
+
+//let answer = +prompt("Есть ли вам 18?", "да");
+//console.log(answer);
+
+//console.log(typeof(answer));
+
+//console.log('arr' + ' - object');
+//console.log(4 + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2"==2);
+
+let IsChecked = true,
+    IsClose = false;
+
+console.log(IsChecked || IsClose);
+
+
